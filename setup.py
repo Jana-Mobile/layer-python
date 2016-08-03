cc09d095971f087b65988ecb4eadbd93b0bd1615
Ross Schlaikjer <ross@jana.com>
2016-08-03 10:20:13 -0400

    Increment version to 0.1.7

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 
 setup(
     name='LayerClient',
-    version='0.1.6',
+    version='0.1.7',
     packages=['LayerClient'],
     description='Client for the Layer Platform API',
     url='https://github.com/Jana-Mobile/layer-python',
